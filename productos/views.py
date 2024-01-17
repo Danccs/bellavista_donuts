5e19e30745c321bcb63a062ad94104e5d697b956
galmarcano <genesismarc@gmail.com>
2024-01-16 23:55:52 -0300

    creando views-templates

--- productos/views.py
+++ productos/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import ProductoForm
+from .models import Producto
 
 # Create your views here.
 def v_list(request):
@@ -9,12 +11,17 @@ def v_list(request):
 
 def v_create(request):
     context = {
-
+        'formulario': ProductoForm()
     }
     return render(request, 'create.html', context)
 
 def v_update(request, producto_id):
-    context = {
+    producto = Producto.objects.get(id = producto_id)
 
-    }
+    if request.method == 'POST':
+        pass
+    else:
+        context = {
+            'formedicion': ProductoForm(instance = producto)
+        }
     return render(request, 'update.html', context)
